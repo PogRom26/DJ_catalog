dce0179b8f5d8342dfb34b934025e9ef1cb17f9d
Roman Pogoreltcev <romzes26@yandex.ru>
2025-11-17 21:54:44 +0700

    Создание модели

--- catalog/models.py
+++ catalog/models.py
@@ -1,16 +1,84 @@
 from django.db import models
 
-class Student(models.Model):
-    first_name = models.CharField(max_length=150, verbose_name='Имя')
-    last_name = models.CharField(max_length=150, verbose_name='Фамилия')
 
-    photo = models.ImageField(upload_to='photos/', verbose_name='Фотография')
-
-    def __str__(self):
-        return f'{self.first_name} {self.last_name}'
+class Category(models.Model):
+    """
+    Модель категории товаров
+    """
+    name = models.CharField(
+        max_length=100,
+        verbose_name='Наименование',
+        help_text='Введите наименование категории'
+    )
+    description = models.TextField(
+        verbose_name='Описание',
+        help_text='Введите описание категории',
+        blank=True,
+        null=True
+    )
 
     class Meta:
-        verbose_name = 'студент'
-        verbose_name_plural = 'студенты'
-        ordering = ['last_name']
-        db_table = 'custom_table_name'
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    """
+    Модель товара
+    """
+    name = models.CharField(
+        max_length=100,
+        verbose_name='Наименование',
+        help_text='Введите наименование товара'
+    )
+    description = models.TextField(
+        verbose_name='Описание',
+        help_text='Введите описание товара',
+        blank=True,
+        null=True
+    )
+    image = models.ImageField(
+        upload_to='products/',
+        verbose_name='Изображение',
+        help_text='Загрузите изображение товара',
+        blank=True,
+        null=True
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        verbose_name='Категория',
+        help_text='Выберите категорию товара',
+        null=True,
+        blank=True
+    )
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name='Цена за покупку',
+        help_text='Введите цену товара'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата создания'
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Дата последнего изменения'
+    )
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        ordering = ['name', 'created_at']
+        permissions = [
+            ("can_edit_category", "Can edit category"),
+            ("can_edit_product", "Can edit product"),
+        ]
+
+    def __str__(self):
+        return f"{self.name} - {self.price}"
