b0740e149213345a57521b5516e941d9d88427f3
Roman Pogoreltcev <romzes26@yandex.ru>
2025-11-17 21:26:16 +0700

    пробное добавление модели

--- catalog/models.py
+++ catalog/models.py
@@ -1,1 +1,16 @@
-# Create your models here.
+from django.db import models
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=150, verbose_name='Имя')
+    last_name = models.CharField(max_length=150, verbose_name='Фамилия')
+
+    photo = models.ImageField(upload_to='photos/', verbose_name='Фотография')
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        verbose_name = 'студент'
+        verbose_name_plural = 'студенты'
+        ordering = ['last_name']
+        db_table = 'custom_table_name'
