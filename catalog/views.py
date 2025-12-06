7b37b16886d7f6b25f355095d56bab25bfde2088
Roman Pogoreltcev <romzes26@yandex.ru>
2025-12-06 19:08:22 +0700

    Вторичное наполнение

--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,15 @@
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from .models import Product
 from .forms import ProductForm
 
 
 class HomeView(ListView):
-    """CBV для главной страницы со списком товаров"""
+    """CBV для главной страницы со списком товаров - доступно всем"""
     model = Product
     template_name = 'catalog/home.html'
     context_object_name = 'products'
@@ -17,7 +21,7 @@ class HomeView(ListView):
 
 
 class ProductDetailView(DetailView):
-    """CBV для детальной страницы товара"""
+    """CBV для детальной страницы товара - доступно всем"""
     model = Product
     template_name = 'catalog/product_detail.html'
     context_object_name = 'product'
@@ -28,12 +32,13 @@ class ProductDetailView(DetailView):
         return context
 
 
-class ProductCreateView(CreateView):
-    """CBV для создания нового товара"""
+class ProductCreateView(LoginRequiredMixin, CreateView):
+    """CBV для создания нового товара - только для авторизованных"""
     model = Product
     form_class = ProductForm
     template_name = 'catalog/product_form.html'
     success_url = reverse_lazy('catalog:home')
+    login_url = '/users/login/'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -42,11 +47,12 @@ class ProductCreateView(CreateView):
         return context
 
 
-class ProductUpdateView(UpdateView):
-    """CBV для редактирования товара"""
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
+    """CBV для редактирования товара - только для авторизованных"""
     model = Product
     form_class = ProductForm
     template_name = 'catalog/product_form.html'
+    login_url = '/users/login/'
 
     def get_success_url(self):
         return reverse_lazy('catalog:product_detail', kwargs={'pk': self.object.pk})
@@ -58,11 +64,12 @@ class ProductUpdateView(UpdateView):
         return context
 
 
-class ProductDeleteView(DeleteView):
-    """CBV для удаления товара"""
+class ProductDeleteView(LoginRequiredMixin, DeleteView):
+    """CBV для удаления товара - только для авторизованных"""
     model = Product
     template_name = 'catalog/product_confirm_delete.html'
     success_url = reverse_lazy('catalog:home')
+    login_url = '/users/login/'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -71,7 +78,7 @@ class ProductDeleteView(DeleteView):
 
 
 class ContactsView(TemplateView):
-    """CBV для страницы контактов"""
+    """CBV для страницы контактов - доступно всем"""
     template_name = 'catalog/contacts.html'
 
     def get_context_data(self, **kwargs):
